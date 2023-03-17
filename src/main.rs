9cee6d34c133e30a326511c3af80614f62fdde87
Martin <marvahus@proton.me>
2023-03-17 16:35:18 +0100

    Update
    - Added commandline arguments
    
    Todo:
    + Repl
    + Parser
    + Interpereter

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ pub mod lexer;
 use lexer::Lexer;
 
 use std::fs;
+use std::vec::Vec;
 
 fn read_file(path: &str) -> String {
 	fs::read_to_string(path).expect("Unable to open file")
 } 
 
 fn main() {
-	let text = read_file("examples/file.lisp");
+	let args: Vec<String> = std::env::args().collect();
+	if args.len() < 2 {
+		todo!("Implement REPL. For now just do `mylisp <file>`");
+	}
+	let path = &args[1];
+	let text = read_file(path);
 	for tok in Lexer::from_iter(text.chars()) {
 		println!("{}", tok);
 	}
